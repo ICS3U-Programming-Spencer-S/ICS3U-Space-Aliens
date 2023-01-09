4f891ba74612ac761f9b23d9bf8fcaea2b70605b
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2023-01-09 19:24:11 +0000

    Created hello world on pybadge

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Spencer Scarlett
+# Date: Jan 9, 2022
+# A basic game called "Horde"
+
+import ugame
+import stage
+
+
+def game_scene():
+    # blank spaces
+    print("\n\n\n")
+    # print name
+    print("Hello world but on a PyBadge")
+
+    # place holder for later???
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
