8a46f6a14d62f5d3db6e1cda5ec26a33830a1050
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2023-01-10 19:10:34 +0000

    Added background to code.py

--- code.py
+++ code.py
@@ -7,12 +7,19 @@
 import ugame
 import stage
 
-
+# function for main scene
 def game_scene():
-    # blank spaces
-    print("\n\n\n")
-    # print name
-    print("Hello world but on a PyBadge")
+
+    # access image bank for PyBadge
+    image_bank_background = stage.Bank.from_bmp16("peter_background.bmp")
+    # uses the background variable and sets its size
+    background = stage.Grid(image_bank_background, 10, 8)
+    # sets the stage to 60 FPS
+    game = stage.Stage(ugame.display, 60)
+    # creates the layer for sprites
+    game.layers = [background]
+    # renders the images
+    game.render_block()
 
     # place holder for later???
     while True:
