1151dd69b9f0f7c7b05d16c1a07d4e67943f6ceb
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2023-01-11 19:13:20 +0000

    Added Sprites

--- code.py
+++ code.py
@@ -4,26 +4,40 @@
 # Date: Jan 9, 2022
 # A basic game called "Horde"
 
-import ugame
 import stage
+import ugame
 
-# function for main scene
+
 def game_scene():
 
     # access image bank for PyBadge
+    # uses the background variable and sets its size
     image_bank_background = stage.Bank.from_bmp16("peter_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
     # uses the background variable and sets its size
     background = stage.Grid(image_bank_background, 10, 8)
+
+    # creates a ship variable sprite at 75x, 66y
+    ship = stage.Sprite(image_bank_sprites, 4, 75, 66)
+
     # sets the stage to 60 FPS
     game = stage.Stage(ugame.display, 60)
-    # creates the layer for sprites
-    game.layers = [background]
-    # renders the images
-    game.render_block()
 
-    # place holder for later???
+    # creates both sprite and background layers
+    game.layers = [ship] + [background]
+
+    # renders the background
+    game.render_block()
+    # Place Holder
     while True:
-        pass
+        # Get user input
+
+        # Update game logic.
+
+        # Redraw sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
