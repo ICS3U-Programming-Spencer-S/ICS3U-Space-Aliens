35b800b4cdea6a03402160f1ac438b0cbf50c236
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2023-01-16 18:11:50 +0000

    Added user input commands

--- code.py
+++ code.py
@@ -15,9 +15,9 @@ def game_scene():
     image_bank_background = stage.Bank.from_bmp16("peter_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # uses the background variable and sets its size
+     # uses the background variable and sets its size
     background = stage.Grid(image_bank_background, 10, 8)
-
+    
     # creates a ship variable sprite at 75x, 66y
     ship = stage.Sprite(image_bank_sprites, 4, 75, 66)
 
@@ -26,19 +26,36 @@ def game_scene():
 
     # creates both sprite and background layers
     game.layers = [ship] + [background]
-
+    
     # renders the background
     game.render_block()
     # Place Holder
     while True:
-        # Get user input
+        # User input commands 
+        keys = ugame.buttons.get_pressed()
 
-        # Update game logic.
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
 
-        # Redraw sprites
+        #Update game logic.
+
+        #Redraw sprites
         game.render_sprites([ship])
         game.tick()
-
 
 if __name__ == "__main__":
     game_scene()
